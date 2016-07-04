1e2b0cb7bf8895a5e7797976eb9c79574b1844a7
cotto89 <cottto89@gmail.com>
2016-07-04 16:20:44 +0900

    clean

--- src/components/SpyableMarkdownPreview.jsx
+++ src/components/SpyableMarkdownPreview.jsx
@@ -1,6 +1,5 @@
 import React, { PropTypes } from 'react';
 
-// TODO: contextからparseOptinoやheadingAttributesを渡せるように修正する
 export default function SpyableMarkdownPreview(props, context) {
   return (
     <div
