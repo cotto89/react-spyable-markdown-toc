4b5646d3f764d33a4e5e31c8182d0c9ab5d760cd
cotto89 <cottto89@gmail.com>
2016-07-04 15:55:00 +0900

    add: SpyableTocItem.jsx
    TocItemを分割

--- src/lib/components/SpyableToc.jsx
+++ src/lib/components/SpyableToc.jsx
@@ -1,42 +1,12 @@
 import React, { PropTypes } from 'react';
-import classNames from 'classnames';
-
-class TocItem extends React.Component {
-  shouldComponentUpdate(nextProps) {
-    return this.props !== nextProps;
-  }
-
-  render() {
-    const { value, depth, index, current, onTocItemClick } = this.props;
-    const cname = classNames({
-      TocItem: true,
-      [`TocItemDepth-${depth}`]: true,
-      'TocItem--current': current,
-    });
-
-    return (
-      <div className={cname} onClick={ev => onTocItemClick(index, ev)}>
-        {value}
-      </div>
-    );
-  }
-}
-
-TocItem.propTypes = {
-  value: PropTypes.string.isRequired,
-  depth: PropTypes.number.isRequired,
-  index: PropTypes.number.isRequired,
-  current: PropTypes.bool.isRequired,
-  onTocItemClick: PropTypes.func.isRequired,
-};
-
+import SpyableTocItem from './SpyableTocItem.jsx';
 
 export default function SpyableToc(props, context) {
   const { currentIndex, onTocItemClick } = context;
   return (
     <div className="SpyableToc">
     {context.headingList.map((x, i) => (
-      <TocItem
+      <SpyableTocItem
         key={i}
         index={i}
         current={currentIndex === i}
